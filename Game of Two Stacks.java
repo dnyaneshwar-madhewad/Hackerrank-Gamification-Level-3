2e1a3bdfa659bfba8aafe84b1832c249af501a20
Dnyaneshwar Madhewad <96038212+dnyaneshwar-madhewad@users.noreply.github.com>
2022-01-13 22:42:36 +0530

    Create Game of Two Stacks.java

---
+++ Game of Two Stacks.java
@@ -0,0 +1,132 @@
+Game of Two Stacks
+Problem
+Submissions
+Leaderboard
+Discussions
+Alexa has two stacks of non-negative integers, stack  and stack  where index  denotes the top of the stack. Alexa challenges Nick to play the following game:
+
+In each move, Nick can remove one integer from the top of either stack  or stack .
+Nick keeps a running sum of the integers he removes from the two stacks.
+Nick is disqualified from the game if, at any point, his running sum becomes greater than some integer  given at the beginning of the game.
+Nick's final score is the total number of integers he has removed from the two stacks.
+Given , , and  for  games, find the maximum possible score Nick can achieve.
+
+Example
+
+
+The maximum number of values Nick can remove is . There are two sets of choices with this result.
+
+Remove  from  with a sum of .
+Remove  from  and  from  with a sum of .
+Function Description
+Complete the twoStacks function in the editor below.
+
+twoStacks has the following parameters: - int maxSum: the maximum allowed sum
+- int a[n]: the first stack
+- int b[m]: the second stack
+
+Returns
+- int: the maximum number of selections Nick can make
+
+Input Format
+
+The first line contains an integer,  (the number of games). The  subsequent lines describe each game in the following format:
+
+The first line contains three space-separated integers describing the respective values of  (the number of integers in stack ),  (the number of integers in stack ), and  (the number that the sum of the integers removed from the two stacks cannot exceed).
+The second line contains  space-separated integers, the respective values of .
+The third line contains  space-separated integers, the respective values of .
+Constraints
+
+Subtasks
+
+ for  of the maximum score.
+Sample Input 0
+
+1
+5 4 10
+4 2 4 6 1
+2 1 8 5
+Sample Output 0
+
+4
+Explanation 0
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'twoStacks' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER maxSum
+     *  2. INTEGER_ARRAY a
+     *  3. INTEGER_ARRAY b
+     */
+
+    public static int twoStacks(int maxSum, List<Integer> a, List<Integer> b) {
+    // Write your code here
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int g = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int gItr = 0; gItr < g; gItr++) {
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int n = Integer.parseInt(firstMultipleInput[0]);
+
+            int m = Integer.parseInt(firstMultipleInput[1]);
+
+            int maxSum = Integer.parseInt(firstMultipleInput[2]);
+
+            String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> a = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int aItem = Integer.parseInt(aTemp[i]);
+                a.add(aItem);
+            }
+
+            String[] bTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> b = new ArrayList<>();
+
+            for (int i = 0; i < m; i++) {
+                int bItem = Integer.parseInt(bTemp[i]);
+                b.add(bItem);
+            }
+
+            int result = Result.twoStacks(maxSum, a, b);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
+
+
+  
+  
+  
+  
